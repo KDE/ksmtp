eb0f166076f5afcb74b67e38b1ff86e6f1a9fa7d
Daniel Vrátil <dvratil@kde.org>
2017-05-04 22:47:56 +0200

    Add missing copyright header to autotests

--- autotests/fakeserver.cpp
+++ autotests/fakeserver.cpp
@@ -1,3 +1,22 @@
+/*
+  Copyright 2010 BetterInbox <contact@betterinbox.com>
+      Author: Christophe Laveault <christophe@betterinbox.com>
+              Gregory Schlomoff <gregory.schlomoff@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "fakeserver.h"
 
 #include <QTest>
