10b3323e12b9721ed9620935c5fdd5105bf9cd10
Alessandro Astone <ales.astone@gmail.com>
2023-12-20 16:47:20 +0100

    Fix the translation template version number (5->6)
    While this module does not need to be co-installable with its Qt5 version,
    change the version suffix for consistency.

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # SPDX-License-Identifier: BSD-3-Clause
 # SPDX-FileCopyrightText: 2017 Montel Laurent <montel@kde.org> 
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/libksmtp5.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/libksmtp6.pot
