18fcb73b6d5f3359f29a63b0fba4b3ef2a2d1b17
Laurent Montel <montel@kde.org>
2025-01-02 06:58:22 +0100

    GIT_SILENT: happy new year

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # SPDX-License-Identifier: BSD-3-Clause
-# SPDX-FileCopyrightText: 2017 Montel Laurent <montel@kde.org> 
+# SPDX-FileCopyrightText: 2017-2025 Laurent Montel <montel@kde.org> 
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/libksmtp6.pot
